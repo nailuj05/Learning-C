55fa20decf1043e3c7195071508ed5600d53589d
Julian <julian.limburg@gmx.de>
2022-08-28 13:11:29 +0200

    FizzBuzz improvements

--- FizzBuzz.c
+++ FizzBuzz.c
@@ -9,24 +9,19 @@ int main()
 
     scanf("%i", &max);
 
-    for (int i = 0; i <= max; i++)
+    for (int i = 1; i <= max; i++)
     {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz\n");
-        }
+        if ((i % 15) == 0)
+            printf("FizzBuzz\t");
+
         else if (i % 3 == 0)
-        {
-            printf("Fizz\n");
-        }
+            printf("Fizz\t");
+
         else if (i % 5 == 0)
-        {
-            printf("Buzz\n");
-        }
+            printf("Buzz\t");
+
         else
-        {
-            printf("%d\n", i);
-        }
+            printf("%d\t", i);
     }
 
     return 0;
