c01b1bc11a249df72e5954c06f01260c06a82a89
Julian <julian.limburg@gmx.de>
2022-08-28 16:21:16 +0200

    Added GetNode()

--- Exercises/LinkedList.c
+++ Exercises/LinkedList.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 // Struct for a Node (contains an item and a pointer to the next Node in the list)
@@ -19,14 +20,39 @@ void append(int item)
     start = node;
 }
 
+struct Node *getNode(int index)
+{
+    struct Node *ptr = start;
+    int counter = 0;
+
+    while (counter != index)
+    {
+        if (ptr->next == NULL)
+        {
+            if (index > 0) // Negativ Index for last element
+            {
+                printf("Index out of bounds\n");
+            }
+            break;
+        }
+
+        ptr = ptr->next;
+        counter++;
+    }
+
+    return ptr;
+}
+
 void printList()
 {
     struct Node *ptr = start;
+    int i = 0;
 
     while (ptr != NULL)
     {
-        printf("[%d]\n", ptr->item);
+        printf("[%i|%d]\n", i, ptr->item);
         ptr = ptr->next;
+        i++;
     }
     printf("[null]\n");
 }
@@ -37,6 +63,8 @@ int main()
     append(24);
     append(42);
     printList();
+
+    printf("%d\n", getNode(4)->item);
 
     return 0;
 }
